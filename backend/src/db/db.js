825a7d4002b22810c1269e16315dd731d4b288dc
Andrey Dolzhenko <adolsheet@gmail.com>
2024-01-29 21:21:25 +0300

    29012024

---
+++ backend/src/db/db.js
@@ -0,0 +1,11 @@
+import { Pool } from "pg";
+
+const pool = new Pool({
+    host: "",
+    user: "",
+    password: "",
+    port: "",
+    database: "",
+});
+
+export default pool;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 async function start () {
     try {
