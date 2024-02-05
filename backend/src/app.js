5948158795a6909278368392c39bed08770fbc9b
Andrey Dolzhenko <adolsheet@gmail.com>
2024-02-05 21:37:57 +0300

    05022024

---
+++ backend/src/app.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+
+import employeesRouter from './modules/employees/index.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+const API = process.env.API || '/api/v1';
+// const URL = `http://127.0.0.1:${PORT}/${API}`;
+
+async function start () {
+    try {
+        // app.get('/', (req, res) => {
+        //     res.send('Hello!');
+        // });
+
+        app.use(`${API}/employees`, employeesRouter);
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://127.0.0.1:${PORT}`);
+        });
+
+    } catch (err) {
+        throw new Error(err);
+    }    
+};
+
+start();
--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,4 +1,13 @@
-import { Pool } from "pg";
+import pkg from 'pg';
+const { Pool } = pkg;
+
+console.log({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: String(process.env.DB_PASSWORD),
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+});
 
 const pool = new Pool({
     host: process.env.DB_HOST,
